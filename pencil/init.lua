eb74c7008e3f0c4adb11085c7a6c27a517fabaa8
Emojigit <55009343+Emojigit@users.noreply.github.com>
2020-07-12 11:17:55 +0800

    Update init.lua

--- pencil/init.lua
+++ pencil/init.lua
@@ -28,8 +28,21 @@ end
 
 register_pencil("pencil:hello_world","Hello World Pencil","Hello World!",nil)
 register_pencil("pencil:blank","Blank Test Pencil",nil,"barrier_inv.png")
-register_pencil("pencil:eraser","Eraser","","eraser.png")
--- Eraser ICON : Everaldo Coelho (YellowIcon); / LGPL (http://www.gnu.org/licenses/lgpl.html)
--- https://commons.wikimedia.org/wiki/File:Crystal128-eraser.svg
+register_pencil("pencil:eraser","Eraser"," ","eraser.png")
+register_pencil("pencil:tmp","TMP Pencil"," ",nil)
 register_pencil("pencil:noaccess","No Access Pencil","Don't Access!",nil)
+
+minetest.register_privilege("add_pencil", {
+	description = "Can use pencil_add command to add pencil."
+})
+math.randomseed(100)
+random = math.random(1000)
+minetest.register_chatcommand("pencil_add", {
+	params = "<text>",
+	privs = {add_pencil=true},
+	description = "Add a pencil.",
+	func = function(name,param)
+		register_pencil(":pencil:tmp",tostring(param).." TMP Pencil",tostring(param),nil)
+	end,
+})
 print("[pencil] Loaded")
