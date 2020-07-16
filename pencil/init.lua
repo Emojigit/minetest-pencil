aac597948ae47caa41d4a97c69fbca24947d346d
Emojigit <55009343+Emojigit@users.noreply.github.com>
2020-07-16 14:18:13 +0800

    Update init.lua

--- pencil/init.lua
+++ pencil/init.lua
@@ -1,6 +1,6 @@
 minetest.log("action","[pencil] Loading")
 pencil = {}
-function pencil.register_pencil(id,name,text,image)
+function pencil.register_pencil(id,name,text,image,not_cre_inv)
 	local id,name,text,image = id,name,text,image
 	if not(id) then
 		print("[pencil] Cannot register a pencil with no name")
@@ -15,6 +15,9 @@ function pencil.register_pencil(id,name,text,image)
 	if not(image) then
 		image = "pencil.png"
 	end
+	if not_cre_inv == nil then
+		not_cre_inv = false
+	end
 	minetest.register_craftitem(id, {
 		description = name,
 		on_use = function(itemstack, user, pointed_thing)
@@ -22,7 +25,8 @@ function pencil.register_pencil(id,name,text,image)
 			local meta = minetest.get_meta(pos);
 			meta:set_string("infotext",text);
 		end,
-		inventory_image = image
+		inventory_image = image,
+		not_in_creative_inventory = not_cre_inv
 	})
 	minetest.log("action","[pencil] Register a pencil "..id.." with text "..text)
 end
@@ -33,12 +37,12 @@ register_pencil = function(id,name,text,image)
 	minetest.log("warning","[pencil] The pencil "..id.." was registered by using \"register_pencil\" , use \"pencil.register_pencil\" instad.")
 end
 
-pencil.register_pencil("pencil:hello_world","Hello World Pencil","Hello World!",nil)
+pencil.register_pencil("pencil:hello_world","Hello World Pencil","Hello World!",nil,false)
 -- test the warning when using the old function to register pencil
-register_pencil("pencil:blank","Blank Test Pencil",nil,"barrier_inv.png")
-pencil.register_pencil("pencil:eraser","Eraser"," ","eraser.png")
-pencil.register_pencil("pencil:tmp","TMP Pencil"," ",nil)
-pencil.register_pencil("pencil:noaccess","No Access Pencil","Don't Access!",nil)
+register_pencil("pencil:blank","Blank Test Pencil",nil,"barrier_inv.png",false)
+pencil.register_pencil("pencil:eraser","Eraser"," ","eraser.png",false)
+pencil.register_pencil("pencil:tmp","TMP Pencil"," ",nil,false)
+pencil.register_pencil("pencil:noaccess","No Access Pencil","Don't Access!",nil,false)
 
 minetest.register_privilege("add_pencil", {
 	description = "Can use pencil_add command to add pencil."
@@ -51,4 +55,32 @@ minetest.register_chatcommand("pencil_add", {
 		pencil.register_pencil(":pencil:tmp",tostring(param).." TMP Pencil",tostring(param),nil)
 	end,
 })
+
+minetest.register_chatcommand("pencil_change", {
+	params = "<text>",
+	privs = {add_pencil=true},
+	description = "Change the text on TMP pencil (pencil:tmp)",
+	func = function(name,param)
+		pencil.register_pencil("pencil:tmp_"..name,name.."\'s Pencil",nil,nil,true)
+	end,
+})
+
+minetest.register_node("pencil:get_tmp",{
+	tiles = {"pencil.png"},
+	is_ground_content = false,
+	on_use = function(itemstack, player, pointed_thing)
+		local receiver = player:get_player_name()
+		local receiverref = player
+		if receiverref == nil then
+			return false, receiver .. " is not a known player"
+		end
+		local leftover = receiverref:get_inventory():add_item("main", "pencil:tmp_"..receiver)
+	end,
+})
+
+minetest.register_on_joinplayer(function(player)
+		print(player:get_player_name())
+		pencil.register_pencil("pencil:tmp_"..player:get_player_name(),player:get_player_name().."\'s Pencil",nil,nil,true)
+	end
+)
 minetest.log("action","[pencil] Loaded")
